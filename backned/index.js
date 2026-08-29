f0b718887c1865afe8ed8cc27b3b68fb8555414d
setthawuttt12 <setthawuttt12@gmail.com>
2026-08-29 09:21:52 +0700

    main

---
+++ backned/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config({path:'.env'})
+const fileUp = require('express-fileupload')
+const express = require('express')
+const path = require('path')
+const JWT_SECRET = process.env.JWT_SECRET
+const cors = require('cors')
+const app = express()
+
+app.use(cors({
+    origin:'http://localhost:3000',
+    credentials:true
+}))
+
+app.use(express.json())
+app.use(fileUp())
+app.use('/uploads',express.static(path.join(__dirname,'./uploads')))
+
+app.use((req,res)=> res.status(404).json({message:"Route not Found"}))
+app.listen(3001,()=>{
+
+    console.log("Server Running on port 3001");
+    
+
+})
